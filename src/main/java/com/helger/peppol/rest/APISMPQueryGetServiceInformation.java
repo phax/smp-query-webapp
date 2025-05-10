f854920e552d933d7da1b0aa2e6840ad18eb0094
Philip Helger <philip@helger.com>
2025-05-10 23:32:22 +0200

    Added suppress warnings

--- src/main/java/com/helger/peppol/rest/APISMPQueryGetServiceInformation.java
+++ src/main/java/com/helger/peppol/rest/APISMPQueryGetServiceInformation.java
@@ -61,6 +61,7 @@ public final class APISMPQueryGetServiceInformation extends AbstractAPIExecutor
 
   private static final Logger LOGGER = LoggerFactory.getLogger (APISMPQueryGetServiceInformation.class);
 
+  @SuppressWarnings ({ "removal", "deprecation" })
   @Override
   public void invokeAPI (@Nonnull final IAPIDescriptor aAPIDescriptor,
                          @Nonnull @Nonempty final String sPath,
