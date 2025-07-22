dd76d0b78fef983062cc3319ecae7a0db7b681de
Philip Helger <philip@helger.com>
2025-07-22 12:41:38 +0200

    Updated

--- build-and-deploy-docker.sh
+++ build-and-deploy-docker.sh
@@ -5,7 +5,7 @@ echo Docker login
 docker login -u phelger
 
 echo Starting buildx
-docker buildx create --name smpqwa
+##docker buildx create --name smpqwa --append
 
 docker buildx build --platform=linux/amd64 --push --pull -t phelger/smpqwa .
 docker buildx build --platform=linux/arm64 --push --pull -t phelger/smpqwa-arm64 .
